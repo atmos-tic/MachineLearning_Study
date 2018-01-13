ba5ea139c3fc45007246ad47e069c5c3c7f06f47
atmos-tic <hantai8118@gmail.com>
2018-01-13 17:12:41 +0900

    [update] update for eigen

--- xor/nural_network.hpp
+++ xor/nural_network.hpp
@@ -3,7 +3,7 @@
 #include <eigen3/Eigen/Dense>
 using namespace Eigen;
 VectorXd step_function(VectorXd x);
-double sigmoid_function(double x);
-double ReLU(double x);
-void softmax(double in[], double out[], int n);
+VectorXd sigmoid_function(VectorXd x);
+VectorXd ReLU(VectorXd x);
+VectorXd softmax(VectorXd x);
 #endif
