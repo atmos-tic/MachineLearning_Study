158054882cbcf6088da650d7c5a494d497e2c58b
atmos-tic <hantai8118@gmail.com>
2018-01-13 16:30:04 +0900

    [fix] intro eigen logic_circuit2

--- xor/logic_circuit.cpp
+++ xor/logic_circuit.cpp
@@ -1,21 +1,39 @@
-// #include "logic_circuit.h"
+#include <iostream>
+#include "logic_circuit.hpp"
+#include <eigen3/Eigen/Dense>
+using namespace Eigen;
+int Logic::AND(int x1, int x2){
+     Vector2d x(x1, x2), w(0.5, 0.5);
+     double b = -0.7;
+     double  tmp = x.dot(w) + b;
+    if(tmp <= 0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
 
-// int AND(int x1, int x2){
-//     double w1 = 0.5, w2 = 0.5, b = -0.7;
-//     double tmp = (double)x1*w1 + (double)x2*w2 + b;
-//     if (tmp <= 0){
-//         return 0;
-//     }else{
-//         return 1;
-//     }
-// }
+int Logic::NAND(int x1, int x2){
+    Vector2d x(x1, x2), w(-0.5, -0.5);
+     double b = 0.7;
+     double  tmp = x.dot(w) + b;
+    if (tmp <= 0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+int Logic::OR(int x1, int x2){
+    Vector2d x(x1, x2), w(0.5, 0.5);
+    double b = -0.2;
+    double  tmp = x.dot(w) + b;
+    if (tmp <= 0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
 
-// int NAND(int x1, int x2){
-//     double w1 = -0.5, w2 = -0.5, b = 0.7;
-//     double tmp = (double)x1*w1 + (double)x2*w2 + b;
-//     if (tmp <= 0){
-//         return 0;
-//     }else{
-//         return 1;
-//     }
-// }
+int Logic::XOR(int x1, int x2){
+    return AND(NAND(x1,x2),OR(x1,x2));
+}
---
+++ xor/logic_circuit.hpp
@@ -0,0 +1,12 @@
+#ifndef LOGIC_CIRCUIT
+#define LOGIC_CIRCUIT
+class Logic
+{
+public:
+    int AND(int x1, int x2);
+    int NAND(int x1, int x2);
+    int OR(int x1, int x2);
+    int XOR(int x1, int x2);
+};
+
+#endif
