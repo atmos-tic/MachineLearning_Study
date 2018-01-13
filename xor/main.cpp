177419462365ce7e66895bfa1917bf6c37c78104
atmos-tic <hantai8118@gmail.com>
2018-01-13 16:29:28 +0900

    [fix] eigen introduce to logic_circuit

--- xor/main.cpp
+++ xor/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
-#include "logic_circuit.h"
+#include "logic_circuit.hpp"
+#include "nural_network.hpp"
 using namespace std;
-
+#include <eigen3/Eigen/Dense>
+using namespace Eigen;
+double network (double);
 int main(){
-   cout <<" AND 00|"<<AND(0,0)<<" 01|"<<AND(0,1)<<" 10|"<<AND(1,0)<<" 11|"<<AND(1,1)<< endl;
-   cout <<"NAND 00|"<<NAND(0,0)<<" 01|"<<NAND(0,1)<<" 10|"<<NAND(1,0)<<" 11|"<<NAND(1,1)<< endl;
-   cout <<"  OR 00|"<<OR(0,0)<<" 01|"<<OR(0,1)<<" 10|"<<OR(1,0)<<" 11|"<<OR(1,1)<< endl;
-   cout <<" XOR 00|"<<XOR(0,0)<<" 01|"<<XOR(0,1)<<" 10|"<<XOR(1,0)<<" 11|"<<XOR(1,1)<< endl;
+    Logic l;
+    cout << l.AND(0,1) << endl;
     char a[10];
     cin >> a;
     return 0;
 }
+// double network (double w[], double b[]){
+
+// }
