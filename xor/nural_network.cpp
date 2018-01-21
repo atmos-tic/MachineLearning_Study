7810f1e6a1377d5b8bf729f0912de7a336609c02
atmos-tic <hantai8118@gmail.com>
2018-01-21 18:56:26 +0900

    [update] back propagation introduce

--- xor/nural_network.cpp
+++ xor/nural_network.cpp
@@ -4,52 +4,100 @@
 using namespace Eigen;
 #include "math.h"
 
-Nuro::Nuro(MatrixXd w, VectorXd b){
-    weight = w;
-    bias = b;
+Afine::Afine(int in_num, int out_num){
+    weight = MatrixXd::Random(out_num, in_num);
+    DATA_NUM = in_num.cols();
+    bias = VectorXd::Zero(DATA_NUM);
 }
-VectorXd step_function(VectorXd x){
-    VectorXd y(x.size());
-    for(int i = 0; i < x.size(); i++){
-        y(i) = x(i) > 0;
-    }
-    return y;
+MatrixXd Afine::forward(MatrixXd x){
+    in = x;
+    out = weight * in + bias * MatrixXd::Ones(DATA_NUM,DATA_NUM);
+    return out;
 }
-
-VectorXd sigmoid_function(VectorXd x){
-    VectorXd y(x.size());
-    for(int i = 0; i < x.size(); i++){
-        y(i) = 1 / (1 + exp(x(i)));
-    }
-    return y;
+MatrixXd Afine::backward(MatrixXd din){
+    MatrixXd dout = weight.transpose() * din;
+    weight += -din * in.transpose() / DATA_NUM;
+    bias += -din * VectorXd::Ones(DATA_NUM);
+    return dout;
 }
 
-VectorXd ReLU(VectorXd x){
-    VectorXd y(x.size());
-    for(int i = 0; i < x.size(); i++){
-        if(x(i) > 0){
-             y(i) = x(i);
-        }else{
-            y(i) = 0;
-        }
-    }
+// class Step{
+//     MatrixXd in, out;
+// public:
+//     MatrixXd forward(MatrixXd x){
+//         in = x;
+//         for(int i = 0; i < x.size(); i++){
+//             out(i) = x(i) > 0;
+//         }
+//         return out;
+//     }
+//     MatrixXd backward(MatrixXd din){
+//         MatrixXd dout = MatrixXd::Zero(din.size(), din.size());
+//         return dout;
+//     }
+// };
+
+MatrixXd Sigmoid::forward(MatrixXd x){
+    in = x;
+    out = 1 / (1 + x.array().exp());
+    return out;
+}
+MatrixXd Sigmoid::backward(MatrixXd din){
+    MatrixXd dout = din.array() * (1.0 - out.array()) * out.array();
+    return dout;
+};
+
+// class ReLU{
+//     MatrixXd in, out;
+// public:
+//     MatrixXd forward(MatrixXd x){
+//         in = x;
+//         out = 1 / (1 + x.array().exp());
+//         return out;
+//     }
+//     MatrixXd backward(MatrixXd din){
+//         MatrixXd dout = din.array() * (1.0 - out.array()) * out.array();
+//         return dout;
+//     }
+//     // VectorXd ReLU(VectorXd x){
+//     //     VectorXd y(x.size());
+//     //     for(int i = 0; i < x.size(); i++){
+//     //         if(x(i) > 0){
+//     //             y(i) = x(i);
+//     //         }else{
+//     //             y(i) = 0;
+//     //         }
+//     //     }
+//     //     return y;
+//     // }
+// };
+
+MatrixXd Softmax_With_Loss::softmax(MatrixXd x){
+    double max = x.maxCoeff();
+    MatrixXd exp_x, y;
+    exp_x = (x - max*MatrixXd::Ones(x.rows(),x.cols())).array().exp();//オーバーフロー対策・最大値を求め全要素から引く
+    y = exp_x/exp_x.sum();
     return y;
 }
-
-VectorXd softmax(VectorXd x){
-    double max = x.maxCoeff(), sum;
-    VectorXd y, exp_x;
-    exp_x = (x - max*VectorXd::Ones(x.size())).array().exp();//オーバーフロー対策・最大値を求め全要素から引く
-    sum = exp_x.sum();
-    y = exp_x/sum;
+double Softmax_With_Loss::cross_entropy_error(MatrixXd y, VectorXd t){
+    MatrixXd delta = MatrixXd::Constant(y.rows(), y.cols(),0.00001);
+    return -(((y + delta).array().log())*(t*VectorXd::Ones(y.size()).transpose()).array()).sum();
+}
+Softmax_With_Loss::Softmax_With_Loss(MatrixXd t){
+    teach = t;
+}
+double Softmax_With_Loss::forward(MatrixXd x){
+    in = x;
+    out = softmax(in);
+    loss = cross_entropy_error(out, teach);
+    return loss;
+}
+MatrixXd Softmax_With_Loss::backward(MatrixXd din){
+    MatrixXd dout = out - teach;
+    return dout;
 }
 
 /*loss function*/
 double mean_squared_error(VectorXd y, VectorXd t){
     return ((y - t).array().pow(2)).sum()/2;
-}
-
-double cross_entropy_error(VectorXd y, VectorXd t){
-    VectorXd delta = VectorXd::Constant(y.size(),0.00001);
-    return -(t.array()*((y + delta).array().log())).sum();
 }
--- xor/nural_network.hpp
+++ xor/nural_network.hpp
@@ -2,17 +2,36 @@
 #define NURO
 #include <eigen3/Eigen/Dense>
 using namespace Eigen;
-class Nuro{
+
+class Afine{
     MatrixXd weight;
     VectorXd bias;
+    MatrixXd in,out;
+    int DATA_NUM;
 public:
-    Nuro(MatrixXd, VectorXd);
+    Afine(int in_num, int out_num);
+    MatrixXd forward(MatrixXd x);
+    MatrixXd backward(MatrixXd din);
 };
-VectorXd step_function(VectorXd x);
-VectorXd sigmoid_function(VectorXd x);
-VectorXd ReLU(VectorXd x);
-VectorXd softmax(VectorXd x);
 
-double mean_squared_error(VectorXd y, VectorXd t);
-double cross_entropy_error(VectorXd y, VectorXd t);
+class Sigmoid{
+    MatrixXd in, out;
+public:
+    MatrixXd forward(MatrixXd x);
+    MatrixXd backward(MatrixXd din);
+};
+
+class Softmax_With_Loss{
+    MatrixXd in, out;
+    double loss;
+    MatrixXd teach;
+private:
+    MatrixXd softmax(MatrixXd x);
+    double cross_entropy_error(MatrixXd y, VectorXd t);
+public:
+    Softmax_With_Loss(MatrixXd t);
+    double forward(MatrixXd x);
+    MatrixXd backward(MatrixXd din);
+};
+
 #endif
