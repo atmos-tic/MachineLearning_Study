10bfb2a907558e29b45cfced5991342509e78cf7
atmos-tic <hantai8118@gmail.com>
2018-03-05 23:43:54 +0900

    [add] svm gradient descent

--- svm/main.cpp
+++ svm/main.cpp
@@ -14,17 +14,31 @@ class SVM{
     
 };
 
-int data1[3][2] = {{9,5},{5,7},{7,6}};
-int data2[3][2] = {{3,4},{1,3},{4,3}};
+#define Ndata (2)
+#define Data_Num (8)
+#define eta_alpha (0.0001)
+#define eta_beta (0.1)
+#define trial (1000)
+Eigen::VectorXd alpha(Data_Num);
+Eigen::VectorXd weight(Ndata);
+double bias;
+Eigen::MatrixXd test_data(Data_Num, Ndata);
+Eigen::VectorXd teach_data(Data_Num);
+
+void SVM_Gradient_Descent(void);
+
 
 int main(void){
+
+    test_data << 9,5, 5,7, 7,6, 8,9, 3,4, 2,3, 4,3, 1,2;
+    teach_data << 1,1,1,1,-1,-1,-1,-1;
 
     cv::Mat img= cv::Mat::zeros(512, 512, CV_8UC3);
     img += cv::Scalar(255, 255 ,255);
     cv::namedWindow("image", cv::WINDOW_AUTOSIZE);
-    for(int i = 0; i < 3; i++){
-        cv::circle(img, cv::Point(data1[i][0]*32, data1[i][1]*32), 10, cv::Scalar(255, 0 ,0), -1);
-        cv::circle(img, cv::Point(data2[i][0]*32, data2[i][1]*32), 10, cv::Scalar(0, 0 ,255), -1);
+    for(int i = 0; i < Data_Num; i++){
+        std::cout<<test_data(i,0)<<test_data(i,1)<<teach_data(i)<<std::endl;
+        cv::circle(img, cv::Point(test_data(i,0)*32, test_data(i,1)*32), 10, cv::Scalar(124*(1+teach_data(i)), 0 ,124*(1-teach_data(i))), -1);
     }
     
     cv::imshow("image", img);
@@ -32,6 +46,16 @@ int main(void){
     cv::waitKey(0);
  
     return 0;
+}
+
+
+void SVM_Gradient_Descent(Eigen::MatrixXd test, Eigen::MatrixXd teach){
+    Eigen::MatrixXd H_Matrix = (teach*Eigen::MatrixXd::Ones(1, Data_Num))*((Eigen::MatrixXd::Ones(Data_Num, 1)*teach.transpose()))*test_data.transpose()*test_data;
+    Eigen::MatrixXd alpha;
+    for(int i = 0; i < trial; i++){
+        alpha = alpha + eta_alpha*((Eigen::MatrixXd::Ones(Data_Num,1)) - H_Matrix*alpha - eta_beta*(teach*Eigen::MatrixXd::Ones(1, Data_Num))*((Eigen::MatrixXd::Ones(Data_Num, 1)*teach.transpose()))*alpha);
+    }
+    std::cout<<alpha<<std::endl;
 }
 
 double SVM::svm_func(Eigen::MatrixXd data){
